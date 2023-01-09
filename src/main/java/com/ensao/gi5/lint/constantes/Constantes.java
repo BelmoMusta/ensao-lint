98e9cd41ca750cd4e620affe257d8e4c362e6fca
Hmama Mohammed <57720200+Hmama-Mohammed@users.noreply.github.com>
2023-01-09 21:31:00 +0100

    Starting the project with 6 rules

--- src/main/java/com/ensao/gi5/lint/constantes/Constantes.java
+++ src/main/java/com/ensao/gi5/lint/constantes/Constantes.java
@@ -4,6 +4,13 @@ public class Constantes {
     public static final String LINT_REG_000 = "LINT_REG_000";
     public static final String LINT_REG_001 = "LINT_REG_001";
 
+    public static final String LINT_REG_018 = "LINT_REG_18";
+    public static final String LINT_REG_016 = "LINT_REG_18";
+    public static final String LINT_REG_015 = "LINT_REG_18";
+    public static final String LINT_REG_007 = "LINT_REG_18";
+    public static final String LINT_REG_005 = "LINT_REG_18";
+    public static final String LINT_REG_010 = "LINT_REG_18";
+
     private Constantes() {
         throw new IllegalStateException("not to be instantiated");
     }
