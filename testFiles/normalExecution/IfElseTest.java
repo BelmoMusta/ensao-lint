22219d6cac2e5719b7ec0eb8e5b7108cb62ddeeb
Benziza <benizizamohamed@gmail.com>
2023-01-12 14:04:32 +0100

    Add test IfElseRuleTest

---
+++ testFiles/normalExecution/IfElseTest.java
@@ -0,0 +1,8 @@
+public class IfElseTest {
+    public void test() {
+        if (true)
+            System.out.println("Hello World");
+        else
+            System.out.println("Goodbye World");
+    }
+}
