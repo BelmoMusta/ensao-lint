4453736ee7b9dd0e9321d2182a5b2266b7d47f04
Benziza <benizizamohamed@gmail.com>
2023-01-12 14:14:28 +0100

    Add test MethodBodyRuleTest

---
+++ testFiles/normalExecution/MethodBodyTest.java
@@ -0,0 +1,57 @@
+public class MethodBodyTest {
+    public void method1() {
+        // This method has 30 lines of code
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+        System.out.println("ff");
+
+    }
+
+    public void method2() {
+        // This method has 31 lines of code
+        // Line 1
+        // Line 2
+        // ...
+        // Line 31
+    }
+}
