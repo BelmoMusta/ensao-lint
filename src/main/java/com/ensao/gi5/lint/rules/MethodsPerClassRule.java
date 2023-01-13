1ca6d529324e686bf0e7f7890494bb06671859e9
Benziza <benizizamohamed@gmail.com>
2023-01-13 21:47:18 +0100

    Add doc to MethodsPerClassRule

--- src/main/java/com/ensao/gi5/lint/rules/MethodsPerClassRule.java
+++ src/main/java/com/ensao/gi5/lint/rules/MethodsPerClassRule.java
@@ -6,6 +6,24 @@ import com.ensao.gi5.lint.wrapper.CompilationUnitWrapper;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
+/**
+
+ This class extends the {@link Rule} class and checks for violations of the rule that states a class should
+
+ not have more than 20 methods. The rule is identified by the constant {@link Constantes#LINT_REG_011}
+
+ and has the highest level of severity.
+
+ The {@link #apply(CompilationUnitWrapper)} method uses the visitor pattern to traverse the abstract syntax tree
+
+ of the compilation unit and checks for classes with more than 20 methods. If a violation is found,
+
+ an instance of {@link Violation} is created with the appropriate description, file name, and line number and is added
+
+ to the list of violations.
+
+ The {@link #isActive()} method is overridden to always return false, indicating that this rule is inactive by default.
+ */
 public class MethodsPerClassRule extends Rule{
     public MethodsPerClassRule() {
         super(Constantes.LINT_REG_011, Level.HIGHEST);
