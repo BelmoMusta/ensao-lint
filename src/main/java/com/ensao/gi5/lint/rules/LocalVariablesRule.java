afb5dd2daa9917770067220ef458321f1e38ac11
Benziza <benizizamohamed@gmail.com>
2023-01-13 21:44:55 +0100

    Add doc to LocalVariablesRule

--- src/main/java/com/ensao/gi5/lint/rules/LocalVariablesRule.java
+++ src/main/java/com/ensao/gi5/lint/rules/LocalVariablesRule.java
@@ -6,6 +6,24 @@ import com.ensao.gi5.lint.wrapper.CompilationUnitWrapper;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
+/**
+
+ This class extends the {@link Rule} class and checks for violations of the rule that states local variables
+
+ should start with a lowercase letter. The rule is identified by the constant {@link Constantes#LINT_REG_003}
+
+ and has the highest level of severity.
+
+ The {@link #apply(CompilationUnitWrapper)} method uses the visitor pattern to traverse the abstract syntax tree
+
+ of the compilation unit and checks for local variable declarations that do not start with a lowercase letter. If a violation is found,
+
+ an instance of {@link Violation} is created with the appropriate description, file name, and line number and is added
+
+ to the list of violations.
+
+ The {@link #isActive()} method is overridden to always return true, indicating that this rule is always active.
+ */
 public class LocalVariablesRule extends Rule {
     public LocalVariablesRule() {
         super(Constantes.LINT_REG_003, Level.HIGHEST);
