3a42b7ed0463668615b995c9160d209e2d14e47e
Benziza <benizizamohamed@gmail.com>
2023-01-13 21:40:21 +0100

    Add doc to EnumRule && Fix doc1

--- src/main/java/com/ensao/gi5/lint/rules/EnumRule.java
+++ src/main/java/com/ensao/gi5/lint/rules/EnumRule.java
@@ -7,6 +7,23 @@ import com.github.javaparser.ast.body.EnumConstantDeclaration;
 import com.github.javaparser.ast.body.EnumDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
+/**
+ This class extends the {@link Rule} class and checks for violations of the rule that states that enumeration elements
+
+ must be in uppercase and use _ as separator. The rule is identified by the constant {@link Constantes#LINT_REG_007}
+
+ and has a low level of severity.
+
+ The {@link #apply(CompilationUnitWrapper)} method uses the visitor pattern to traverse the abstract syntax tree
+
+ of the compilation unit and checks for enumeration elements that are not in uppercase or use _ as separator. If a violation is found,
+
+ an instance of {@link Violation} is created with the appropriate description, file name, and line number and is added
+
+ to the list of violations.
+
+ The {@link #isActive()} method is overridden to always return true, indicating that this rule is always active.
+ */
 public class EnumRule extends Rule{
     public EnumRule() {
         super(Constantes.LINT_REG_007, Level.LOW);
