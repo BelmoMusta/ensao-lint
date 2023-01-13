9c21285ffb28d0f5b5b638915850fe192b4ea66a
Hafida-BOUMESHOUL <hafida.boumeshoul@gmail.com>
2023-01-13 16:52:24 +0100

    linter updates

--- src/test/java/com/ensao/gi5/lint/rules/CatchLogExceptionsTest.java
+++ src/test/java/com/ensao/gi5/lint/rules/CatchLogExceptionsTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
