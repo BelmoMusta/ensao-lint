330a1611bf160b60ade11d4fa7d377d833bc9fdf
BOUCHENTOUF Oussama <oussamabouchentouf6@gmail.com>
2023-01-14 13:26:43 +0100

    Adding outputPrinter for : CSV, HTML, JSON, MARKDOWN

--- src/main/java/com/ensao/gi5/lint/runner/Runner.java
+++ src/main/java/com/ensao/gi5/lint/runner/Runner.java
@@ -1,20 +1,32 @@
 package com.ensao.gi5.lint.runner;
 
 import com.ensao.gi5.lint.Linter;
-import com.ensao.gi5.lint.printer.ConsolePrinter;
+import com.ensao.gi5.lint.printer.*;
 import com.ensao.gi5.lint.rules.*;
+
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez le format output souhaité : ");
+
         String directory = "testFiles/normalExecution/Example.java";
+        String outputType = scanner.nextLine();
+
 
         for (int i = 0; i < args.length; i++) {
             String argument = args[i];
             if (argument.equals("-s")) {
                 if (i == args.length - 1 || (directory = args[i + 1]).isEmpty()) {
-                    System.out.println("Usage example : -s  D:/Test/Example.java");
+                    System.out.println("Usage example : -s  testFiles/normalExecution/Example.java");
                     throw new IllegalStateException("The directory or file are not specified");
+                }
+            } else if (argument.equals("-o")) {
+                if (i == args.length - 1 || (outputType = args[i + 1]).isEmpty()) {
+                    System.out.println("Usage example : -o json");
+                    throw new IllegalStateException("The output type is not specified");
                 }
             }
         }
@@ -27,8 +39,30 @@ public class Runner {
         linter.registerRule(new ClearVisibilityOfClasses());
         linter.registerRule(new MethodBody());
         linter.registerRule(new MethodsNumbersLessThanTwinty());
-        linter.registerPrinter(new ConsolePrinter());
+
         linter.registerSource(directory);
+
+        switch (outputType) {
+            case "console":
+                linter.registerPrinter(new ConsolePrinter());
+                break;
+            case "csv":
+                linter.registerPrinter(new CSVFilePrinter());
+                break;
+            case "json":
+                linter.registerPrinter(new JSONFilePrinter());
+                break;
+            case "html":
+                linter.registerPrinter(new HTMLFilePrinter());
+               break;
+            case "markdown":
+                linter.registerPrinter(new MARKDOWNFilePrinter());
+                break;
+            default:
+                System.out.println("Invalid output type, valid options are: console, csv, json, html, markdown");
+                return;
+        }
+
         linter.run();
     }
 }
