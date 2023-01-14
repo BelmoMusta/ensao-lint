dfbaa5254437a789b02efedbfaa7f5bec5dd95d2
ZOUBEIR KASMI <kasmizoubeir@gmail.com>
2023-01-14 08:25:21 +0100

    Test008

---
+++ testFiles/normalExecution/ruleHuit.java
@@ -0,0 +1,70 @@
+public class ruleHuit{
+	 public void method1() {
+		 
+		 //30 lines of code 
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+		 System.out.println("hello world");
+	 }
+	  public void method2() {
+		  //31 lines of code
+		  System.out.println("hello world");
+		  System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+			 System.out.println("hello world");
+	  }
+}
